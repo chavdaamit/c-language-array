d3c16866cb64de333cc7d9ddf798798f71f7ee2e
chavda amit <adchavda7777@gmail.com>
2025-11-26 16:30:26 +0530

    Add files via upload

---
+++ 10 - chapter/static_insertation.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    int num[5] = {5,4,7,8,9};
+
+
+    int number[5] ={};
+
+
+
+    number[0] = 3;
+
+    number[1]=5;
+
+    number[2] =2;
+
+    number[3] = 8;
+
+    number[4] =6;
+
+    printf("%d",number[2]);
+
+
+
+    return 0;
+}
