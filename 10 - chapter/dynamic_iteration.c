b67590e189ed30beb82103e3bf432bfe433ef983
chavda amit <adchavda7777@gmail.com>
2025-11-27 16:17:55 +0530

    Add files via upload

---
+++ 10 - chapter/dynamic_iteration.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+
+// int main(){
+
+
+//     int num[5]={2,3,5,6,7};
+
+
+// for (int i=0;i<=4;i++){
+
+// printf("%d\n",num[i]);
+
+
+// }
+
+
+
+
+//     return 0;
+// }
+
+
+
+#include <stdio.h>
+
+int main(){
+
+int num[5]={111,122,133,144,155};
+
+
+for(int i=0;i<=4;i++){
+
+    printf("%d\n",num[i]);
+}
+
+
+
+return 0;
+
+}
---
+++ 10 - chapter/static_iteration.c
@@ -0,0 +1,43 @@
+// #include <stdio.h>
+
+// int main(){
+
+
+
+//     int num[5]={9,5,4,3,2};
+
+
+
+//     printf("%d\n",num[0]);
+//     printf("%d\n",num[1]);
+//     printf("%d\n",num[2]);
+//     printf("%d\n",num[3]);
+//     printf("%d\n",num[4]);
+
+
+
+//     return 0;
+// }
+
+
+
+#include <stdio.h>
+
+int main(){
+
+
+int num[5]={11,12,13,14,15};
+
+
+printf("%d\n",num[0]);
+printf("%d\n",num[1]);
+printf("%d\n",num[2]);
+printf("%d\n",num[3]);
+printf("%d\n",num[4]);
+
+
+
+
+    return 0;
+
+}
