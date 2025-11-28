aecf5fa83813d2531e58f68b9fdb9ded1313fb60
chavda amit <adchavda7777@gmail.com>
2025-11-28 16:41:25 +0530

    Add files via upload

---
+++ 11 2d array/iteration.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num[3][3] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9}};
+
+    printf("%d\n", num[0][0]);
+    printf("%d\n", num[0][1]);
+    printf("%d\n", num[0][2]);
+    printf("%d\n", num[1][0]);
+    printf("%d\n", num[1][1]);
+    printf("%d\n", num[1][2]);
+    printf("%d\n", num[2][0]);
+    printf("%d\n", num[2][1]);
+    printf("%d\n", num[2][2]);
+
+    return 0;
+}
